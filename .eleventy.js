fab1aa50ac3bbe66253888c6219e6247aa195296
Craig - qrayg <craig@craigerskine.com>
2022-06-21 13:59:51 -0500

    ...

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/_redirects");
   eleventyConfig.addPassthroughCopy("src/_assets");
+  eleventyConfig.addPassthroughCopy("src/favicon.ico");
   return {
     markdownTemplateEngine: "njk",
     htmlTemplateEngine: "njk",
