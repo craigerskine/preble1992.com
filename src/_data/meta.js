6b86ccfc0911db567ad1398b2e480b45d03a297e
Craig - qrayg <craig@craigerskine.com>
2022-06-22 15:15:12 -0500

    ...

--- src/_data/meta.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  title: 'Preble Class of 1992: 30th Reunion',
-  description: 'Preble High School class of 1992 reunion information',
-  author: 'Craig Erskine',
-  author_url: 'craigerskine.com',
-}
---
+++ src/_data/site.js
@@ -0,0 +1,20 @@
+module.exports = {
+  meta: {
+    title: 'Preble Class of 1992: 30th Reunion',
+    description: 'Preble High School class of 1992 reunion information',
+    author: 'Craig Erskine',
+    author_url: 'craigerskine.com',
+  },
+  social: [
+    {
+      title: 'Facebook',
+      url: 'https://www.facebook.com/preblehigh92',
+      ico: 'facebook-f',
+    },
+    // {
+    //   title: 'PayPal',
+    //   url: '#',
+    //   ico: 'paypal',
+    // },
+  ],
+}
