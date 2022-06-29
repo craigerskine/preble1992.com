ba1ca463e3db1995b38573cde0fce69a2ebe0b9b
Craig - qrayg <craig@craigerskine.com>
2022-06-28 21:04:23 -0500

    new design

--- src/_data/home.js
+++ src/_data/home.js
@@ -18,5 +18,28 @@ module.exports = {
   details: {
     heading: 'The schedule is jam-packed with smart, creative, and evil activities',
     lead: 'The worst people from our school will be planning this... so don\'t get your hopes up. Either way though, it should be a fun time and you should really show up.',
+    details: [
+      {
+        label: 'Date',
+        info: 'November 5th, 2022',
+        sub: '@7pm - ???<br />Please consider donating<br /><small>(coming soon)</small>&nbsp;',
+        url: '/_assets/cal/preble-event.ics',
+        ico: 'fa-solid fa-calendar',
+      },
+      {
+        label: 'Location',
+        info: 'The Woods Golf Course',
+        sub: '530 Erie Rd.<br />Green Bay, WI<br />54311',
+        url: 'https://golfthewoods.com/facilities',
+        ico: 'fa-solid fa-location-dot',
+      },
+      {
+        label: 'Do your part',
+        info: 'Spread the news',
+        sub: 'Tell all your classmates...<br />friend, foe, whatever',
+        url: 'https://facebook.com/sharer.php?u=https://preble1992.com',
+        ico: 'fa-brands fa-facebook-f',
+      },
+    ],
   },
 }
