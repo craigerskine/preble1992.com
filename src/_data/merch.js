115d9d11a45d8e9472ff313110eb0eea507aa7ed
Craig - qrayg <craig@craigerskine.com>
2022-08-09 20:29:03 -0500

    ...

---
+++ src/_data/merch.js
@@ -0,0 +1,18 @@
+module.exports = function() {
+  return {
+    items: [
+      {
+        title: 'Shirt',
+        img: 'shirt.png',
+      },
+      {
+        title: 'Sticker',
+        img: 'sticker.jpg',
+      },
+      {
+        title: 'Mug',
+        img: 'mug.jpg',
+      },
+    ],
+  }
+}
