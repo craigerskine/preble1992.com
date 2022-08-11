b9ebba3257cf15bcb4ebd3908de98a844780ec4a
Craig - qrayg <craig@craigerskine.com>
2022-08-11 08:41:08 -0500

    ...

--- src/_data/merch.js
+++ src/_data/merch.js
@@ -3,15 +3,18 @@ module.exports = function() {
     items: [
       {
         title: 'Shirt',
-        img: 'shirt.png',
+        img: 'shirt.jpg',
+        url: 'https://teepublic.com/t-shirt/34031441-preble-92',
       },
       {
         title: 'Sticker',
         img: 'sticker.jpg',
+        url: 'https://teepublic.com/sticker/34031441-preble-92',
       },
       {
         title: 'Mug',
         img: 'mug.jpg',
+        url: 'https://teepublic.com/mug/34031441-preble-92',
       },
     ],
   }
