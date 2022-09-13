ccc4b8e04618199d1f1deb17c99ebc06b112e795
Craig - qrayg <craig@craigerskine.com>
2022-09-13 09:17:46 -0500

    ...

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,29 @@
+const { EleventyRenderPlugin } = require("@11ty/eleventy");
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+const markdownIt = require("markdown-it");
+const markdownItAttrs = require("markdown-it-attrs");
 const yaml = require("js-yaml");
 
 module.exports = function(eleventyConfig) {
 
-  // .yaml extension in _data
-  eleventyConfig.addDataExtension('yaml', (contents) => yaml.load(contents));
-
   eleventyConfig.addPassthroughCopy('_site/_redirects');
   eleventyConfig.addPassthroughCopy('_site/_assets');
   eleventyConfig.addPassthroughCopy('_site/favicon.ico');
+
+  eleventyConfig.addPlugin(EleventyRenderPlugin);
+  eleventyConfig.addPlugin(eleventyNavigationPlugin);
+
+  //{% renderTemplate "md" %}
+  //# Blah{.text-center}
+  //{% endrenderTemplate %}
+  let markdownLibrary = markdownIt().disable('code').use(markdownItAttrs);
+  eleventyConfig.setLibrary('md', markdownLibrary);
+
+  eleventyConfig.addDataExtension('yaml', (contents) => yaml.load(contents));
+
   eleventyConfig.addShortcode('version', () => `${String(Date.now())}`);
   eleventyConfig.addShortcode('year', () => `${new Date().getFullYear()}`);
+
   // | randomLimit(6, page.url)
   eleventyConfig.addFilter('randomLimit', (arr, limit, currPage) => {
     const pageArr = arr.filter((page) => page.url !== currPage);
@@ -18,7 +32,14 @@ module.exports = function(eleventyConfig) {
     });
     return pageArr.slice(0, limit);
   });
+  // for item in (items | flatMap('category') | unique('category'))
+  eleventyConfig.addFilter('flatMap', (list, key) => list.flatMap((x) => x[key]));
+  eleventyConfig.addFilter('unique', (list, key) => {
+    const map = new Map(list.map((x) => [x[key], x]))
+    return [...map.values()]
+  });
   return {
+    jsDataFileSuffix: '.data',
     markdownTemplateEngine: 'njk',
     htmlTemplateEngine: 'njk',
     dir: {
