2c5a10569273d24209d5bdd7571498bc5635ce72
Craig - qrayg <craig@craigerskine.com>
2023-02-24 15:12:44 -0600

    11ty 2.0

---
+++ _site/_app/_app.js
@@ -0,0 +1,36 @@
+import 'instant.page';
+
+import { install, injectGlobal } from '@twind/core';
+import presetAutoprefix from '@twind/preset-autoprefix';
+import presetTailwind from '@twind/preset-tailwind';
+
+install({
+  presets: [presetAutoprefix(), presetTailwind()],
+  darkMode: 'class',
+  hash: false,
+  theme: {
+    extend: {
+      colors: ({ theme }) => ({
+        gray: theme('colors.stone'),
+        pri: theme('colors.green'),
+        sec: theme('colors.yellow'),
+      }),
+      fontFamily: ({ theme }) => ({
+        serif: 'Roboto Slab,'+ theme('fontFamily.serif'),
+      }),
+    },
+  },
+  rules: [
+    [ 'scroll-smooth', { 'scroll-behavior': 'smooth' } ],
+  ],
+});
+
+injectGlobal({
+  // global
+  '[x-cloak]': { '@apply': 'hidden', },
+});
+
+import Alpine from 'alpinejs';
+window.Alpine = Alpine;
+
+Alpine.start();
